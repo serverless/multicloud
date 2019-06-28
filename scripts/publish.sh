8df9ef01e35a3d51c110bf3a8312693661f5cdfe
P.J. Little <pelittle@microsoft.com>
2019-06-28 18:31:05 +0000

    Merged PR 33: ci: tag prereleases from dev branch
    ci: tag prereleases from dev branch
    
    Added prerelease pipelines for each package. Tags pushed
    to `dev` branch will publish beta releases to NPM.
    NOTE: version bumps in package.json are not yet automated
    and still need to be done manually.
    
    AB#335
    
    Related work items: #335

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
-set -euo pipefail
+set -eo pipefail
 
 ./build.sh
+
+## configure npm to sign commit
+npm config set sign-git-tag true
+
 # NOTE: auth is taken care of via AzDO `npm auth` task. ENV vars would work as well.
-npm publish
+if [ -z "$1" ]; then
+  echo "Publishing 'latest' to NPM...";
+  npm publish
+else
+  echo "Publishing 'prerelease' to NPM...";
+  npm publish --tag=beta
+fi
