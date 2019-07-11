5bdf623e613635c37225090d782a3fa654fba6cf
azuredevops@microsoft.com <Azure Pipelines>
2019-07-11 16:47:56 -0700

    Updated version script to take optional NPM version as argument

--- scripts/version.sh
+++ scripts/version.sh
@@ -1,13 +1,25 @@
 #!/bin/bash
 set -euo pipefail
 
+PACKAGE_NAME=$1
+NPM_VERSION=${2-""}
+
 # Get full branch name excluding refs/head from the env var SOURCE_BRANCH
 SOURCE_BRANCH_NAME=${SOURCE_BRANCH/refs\/head\/}
 
 # Configure git to commit as Azure Dev Ops
 git config --local user.email "Azure Pipelines"
 git config --local user.name "azuredevops@microsoft.com"
-NPM_VERSION=`npm version prerelease`
+
+echo v=${NPM_VERSION}
+
+if ["$NPM_VERSION" == ""]; then
+    echo Version was not passed
+    NPM_VERSION=`npm version prerelease`
+else
+    echo Version ${NPM_VERSION} was passed
+    NPM_VERSION=`npm version ${NPM_VERSION}`
+fi
 
 # Stage update to package.json files
 git add package.json
@@ -16,6 +28,6 @@ git add package-lock.json
 # Since there isn't a package.json at the root of repo
 # and we have multiple packages within same repo
 # we need to manually commit and tag in order to create unique tag names
-git commit -m "Bumping NPM package $1 prerelease to version ${NPM_VERSION} ***NO_CI***"
-git tag $1-${NPM_VERSION}
+git commit -m "Bumping NPM package ${PACKAGE_NAME} prerelease to version ${NPM_VERSION} ***NO_CI***"
+git tag ${PACKAGE_NAME}-${NPM_VERSION}
 git push origin HEAD:${SOURCE_BRANCH_NAME} --tags
