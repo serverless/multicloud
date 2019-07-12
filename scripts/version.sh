11d3d3f1fcd318fd71b8e1532fe3379842128216
Wallace Breza (MSFT) <wabrez@microsoft.com>
2019-07-12 18:33:17 +0000

    Merged PR 69: feat: Pipeline to manually queue NPM release
    - Pipeline can be manually queued to queue an NPM package from any branch for any release type.
    - Pipeline expected param of `major` | `minor` | `patch` | `prerelease`
    - Defaults to `prerelease`
    
    Related work items: #471, #472

--- scripts/version.sh
+++ scripts/version.sh
@@ -2,7 +2,7 @@
 set -euo pipefail
 
 PACKAGE_NAME=$1
-NPM_VERSION=${2-""}
+NPM_RELEASE_TYPE=${2-"prerelease"}
 
 # Get full branch name excluding refs/head from the env var SOURCE_BRANCH
 SOURCE_BRANCH_NAME=${SOURCE_BRANCH/refs\/head\/}
@@ -12,11 +12,9 @@ git config --local user.email "Azure Pipelines"
 git config --local user.name "azuredevops@microsoft.com"
 git pull origin ${SOURCE_BRANCH_NAME}
 
-if ["$NPM_VERSION" == ""]; then
-    NPM_VERSION=`npm version prerelease`
-else
-    NPM_VERSION=`npm version ${NPM_VERSION}`
-fi
+NPM_VERSION=`npm version ${NPM_RELEASE_TYPE}`
+
+echo ${NPM_VERSION}
 
 # Stage update to package.json files
 git add package.json
@@ -25,6 +23,6 @@ git add package-lock.json
 # Since there isn't a package.json at the root of repo
 # and we have multiple packages within same repo
 # we need to manually commit and tag in order to create unique tag names
-git commit -m "Bumping NPM package ${PACKAGE_NAME} prerelease to version ${NPM_VERSION} ***NO_CI***"
+git commit -m "Bumping NPM package ${PACKAGE_NAME} prerelease to version ${NPM_VERSION}"
 git tag ${PACKAGE_NAME}-${NPM_VERSION}
 git push origin HEAD:${SOURCE_BRANCH_NAME} --tags
