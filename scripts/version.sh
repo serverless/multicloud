8e1df2b39d0c1a4b8de5a60d1d66f39e78d607c3
Wallace Breza <wallace@breza.me>
2019-07-12 16:09:39 -0700

    testing

--- scripts/version.sh
+++ scripts/version.sh
@@ -7,22 +7,16 @@ NPM_RELEASE_TYPE=${2-"prerelease"}
 # Get full branch name excluding refs/head from the env var SOURCE_BRANCH
 SOURCE_BRANCH_NAME=${SOURCE_BRANCH/refs\/head\/}
 
-printenv | sort
-
-git branch
-git remote -v
-
-
 # Configure git to commit as Azure Dev Ops
 git config --local user.email "Azure Pipelines"
 git config --local user.name "azuredevops@microsoft.com"
 
-git checkout origin ${SOURCE_BRANCH_NAME}
-git pull
+git pull origin ${SOURCE_BRANCH_NAME}
+git checkout ${SOURCE_BRANCH_NAME}
 echo Checked out branch: ${SOURCE_BRANCH_NAME}
 
 NPM_VERSION=`npm version ${NPM_RELEASE_TYPE}`
-echo Set NPM version to ${NPM_VERSION}
+echo Set NPM version to: ${NPM_VERSION}
 
 # Stage update to package.json files
 git add package.json
@@ -32,7 +26,9 @@ git add package-lock.json
 # and we have multiple packages within same repo
 # we need to manually commit and tag in order to create unique tag names
 git commit -m "Bumping NPM package ${PACKAGE_NAME} prerelease to version ${NPM_VERSION} ***NO_CI***"
+SHA=`git rev-parse HEAD`
+
 git tag ${PACKAGE_NAME}-${NPM_VERSION}
 git push --tags
 
-echo Pushed new tag @ ${PACKAGE_NAME}-${NPM_VERSION}
+echo Pushed new tag: ${PACKAGE_NAME}-${NPM_VERSION} with SHA: ${SHA:0:6}
