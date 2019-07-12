4cb394a48adbe7fd0ece9a9526b7ced72b96a97d
Wallace Breza (MSFT) <wabrez@microsoft.com>
2019-07-12 23:49:55 +0000

    Merged PR 75: fix: Ensure latest is pulled and checkout out before version
    When changes are made on dev branch across projects (core/azure/aws) All builds are queued with the same SHA.  However - during the bump/publish we are making new commits, so subsequent previously scheduled builds need to ensure they have the latest otherwise we get git merge errors.
    
    - [x] Pull latest from origin
    - [x] Checkout target branch
    - [x] Apply NPM version
    
    Related work items: #472

--- scripts/version.sh
+++ scripts/version.sh
@@ -5,16 +5,18 @@ PACKAGE_NAME=$1
 NPM_RELEASE_TYPE=${2-"prerelease"}
 
 # Get full branch name excluding refs/head from the env var SOURCE_BRANCH
-SOURCE_BRANCH_NAME=${SOURCE_BRANCH/refs\/head\/}
+SOURCE_BRANCH_NAME=${SOURCE_BRANCH/refs\/heads\/}
 
 # Configure git to commit as Azure Dev Ops
 git config --local user.email "Azure Pipelines"
 git config --local user.name "azuredevops@microsoft.com"
+
 git pull origin ${SOURCE_BRANCH_NAME}
+git checkout ${SOURCE_BRANCH_NAME}
+echo Checked out branch: ${SOURCE_BRANCH_NAME}
 
 NPM_VERSION=`npm version ${NPM_RELEASE_TYPE}`
-
-echo ${NPM_VERSION}
+echo Set NPM version to: ${NPM_VERSION}
 
 # Stage update to package.json files
 git add package.json
@@ -24,5 +26,9 @@ git add package-lock.json
 # and we have multiple packages within same repo
 # we need to manually commit and tag in order to create unique tag names
 git commit -m "Bumping NPM package ${PACKAGE_NAME} prerelease to version ${NPM_VERSION} ***NO_CI***"
+SHA=`git rev-parse HEAD`
+
 git tag ${PACKAGE_NAME}-${NPM_VERSION}
-git push origin HEAD:${SOURCE_BRANCH_NAME} --tags
+git push origin ${SOURCE_BRANCH_NAME} --tags
+
+echo Pushed new tag: ${PACKAGE_NAME}-${NPM_VERSION} @ SHA: ${SHA:0:8}
