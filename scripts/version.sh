0f57552584b771ff38c3796cc00d48c6bab93005
Wallace Breza <wallace@breza.me>
2019-07-12 16:18:01 -0700

    testing

--- scripts/version.sh
+++ scripts/version.sh
@@ -7,7 +7,7 @@ NPM_RELEASE_TYPE=${2-"prerelease"}
 echo SOURCE_BRANCH=${SOURCE_BRANCH}
 
 # Get full branch name excluding refs/head from the env var SOURCE_BRANCH
-SOURCE_BRANCH_NAME=${SOURCE_BRANCH/refs\/head\/}
+SOURCE_BRANCH_NAME=${SOURCE_BRANCH/refs\/heads\/}
 
 echo SOURCE_BRANCH_NAME=${SOURCE_BRANCH_NAME}
 
