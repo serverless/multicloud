d9fb6318e7ac117d5f556cab60263f8a0d6588e5
Wallace Breza <wallace@breza.me>
2019-07-12 15:50:56 -0700

    More debugging

--- scripts/version.sh
+++ scripts/version.sh
@@ -7,6 +7,7 @@ NPM_RELEASE_TYPE=${2-"prerelease"}
 # Get full branch name excluding refs/head from the env var SOURCE_BRANCH
 SOURCE_BRANCH_NAME=${SOURCE_BRANCH/refs\/head\/}
 
+git branch
 git remote -v
 
 echo Checking out ${SOURCE_BRANCH_NAME}
@@ -14,7 +15,7 @@ echo Checking out ${SOURCE_BRANCH_NAME}
 # Configure git to commit as Azure Dev Ops
 git config --local user.email "Azure Pipelines"
 git config --local user.name "azuredevops@microsoft.com"
-git checkout ${SOURCE_BRANCH_NAME}
+git checkout origin ${SOURCE_BRANCH_NAME}
 git pull
 
 NPM_VERSION=`npm version ${NPM_RELEASE_TYPE}`
@@ -30,4 +31,4 @@ git add package-lock.json
 # we need to manually commit and tag in order to create unique tag names
 git commit -m "Bumping NPM package ${PACKAGE_NAME} prerelease to version ${NPM_VERSION} ***NO_CI***"
 git tag ${PACKAGE_NAME}-${NPM_VERSION}
-git push --tags
+git push origin ${SOURCE_BRANCH_NAME} --tags
