914f87e577d98b62e27da48ef8c17a174bee734c
Tanner Barlow (MSFT) <tabarlow@microsoft.com>
2019-07-22 19:03:20 +0000

    Merged PR 105: feat: Added runtime to CloudContext type and added JS Docs
    Adds `runtime` as an attribute to the `CloudContext` type with JSDocs for intellisense

--- core/src/cloudContext.ts
+++ core/src/cloudContext.ts
@@ -6,16 +6,30 @@ import { CloudService } from "./services/cloudService";
 import { TelemetryService } from "./services/telemetry";
 import { CloudContainer } from "./cloudContainer";
 
+/**
+ * Common context for Serverless functions
+ */
 export interface CloudContext {
+  /** Cloud provider type */
   providerType: string;
+  /** Request ID */
   id: string;
+  /** Container for Cloud Services */
   container?: CloudContainer;
+  /** Common Request for Serverless Functions */
   req?: CloudRequest;
+  /** Common Response for Serverless Functions */
   res?: CloudResponse;
+  /** Storage Service */
   storage?: CloudStorage;
+  /** Logging Service */
   logger?: Logger;
+  /** Invocation Service */
   service?: CloudService;
+  /** Original cloud-specific event context */
   runtime?: any;
+  /** Telemetry Service */
   telemetry?: TelemetryService;
+  /** Send response */
   send: (body: any, status: number) => void;
 }
