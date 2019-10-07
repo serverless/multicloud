27a02be7883356776a8e952fd869bcff807d89e8
Leandro Gaspari <leandro.gaspari@southworks.com>
2019-10-07 16:07:45 -0300

    fix: Update LambdaCloudService response
    Its required to have the same response for the CloudServices
    implementations, meaning that the result should be the data of T type
    instead of the whole Lambda Invoke response.
    
    AB#1236

--- aws/src/services/lambdaCloudService.test.ts
+++ aws/src/services/lambdaCloudService.test.ts
@@ -1,6 +1,7 @@
 import AWS from "aws-sdk";
+import { Readable } from "stream";
 import { LambdaCloudService, AWSCloudServiceOptions, AWSInvokeType } from ".";
-import { CloudContainer } from "@multicloud/sls-core";
+import { CloudContainer, CloudContext, CloudContextBuilder, convertToStream } from "@multicloud/sls-core";
 
 jest.mock("aws-sdk");
 
@@ -8,88 +9,111 @@ const getCartAWSCloudService: AWSCloudServiceOptions = {
   name: "aws-getCart",
   region: "west-us-2",
   arn: "arn-west-us-2-getCart",
-}
+};
 
 describe("LambdaCloudService", () => {
-  let container: CloudContainer;
-  let context;
+  const container = new CloudContainer();
+  container.bind(getCartAWSCloudService.name).toConstantValue(getCartAWSCloudService);
 
-  beforeAll(() => {
-    container = new CloudContainer();
-    context = {
-      container
-    }
-    container.bind(getCartAWSCloudService.name).toConstantValue(getCartAWSCloudService);
+  const builder = new CloudContextBuilder();
+  const context: CloudContext = {
+    ...builder.build(),
+    container
+  };
+
+  const invokeResponseBody = {
+    foo: "bar"
+  };
+
+  let invokeResponse;
+  beforeEach(() => {
+    invokeResponse = convertToStream(
+      JSON.stringify({
+        body: JSON.stringify(invokeResponseBody)
+      })
+    );
 
     AWS.Lambda.prototype.invoke = jest.fn().mockReturnValue({
-      promise: jest.fn().mockReturnValue(Promise.resolve)
-    })
+      createReadStream: jest.fn().mockReturnValue(invokeResponse)
+    });
   });
 
   it("should create a Lambda instance", async () => {
     const sut = new LambdaCloudService(context);
-    await sut.invoke<Promise<any>>("aws-getCart", null, {});
+    await sut.invoke<any>("aws-getCart", null, {});
     expect(AWS.Lambda).toHaveBeenCalled();
-  })
+  });
 
   it("should be called as fireAndWait by default", async () => {
+    const payload = {};
     const options = {
-      FunctionName: "arn-west-us-2-getCart",
-      Payload: {},
+      FunctionName: getCartAWSCloudService.arn,
+      Payload: JSON.stringify({ body: payload }),
       InvocationType: AWSInvokeType.fireAndWait
-    }
+    };
 
     const sut = new LambdaCloudService(context);
-    await sut.invoke<Promise<any>>("aws-getCart", null, {});
+    await sut.invoke<any>("aws-getCart", null, payload);
     expect(AWS.Lambda.prototype.invoke).toHaveBeenCalledWith(options);
-  })
+  });
 
   it("should return an empty response on fireAndForget", async () => {
+    const payload = {};
     const options = {
-      FunctionName: "arn-west-us-2-getCart",
-      Payload: {},
+      FunctionName: getCartAWSCloudService.arn,
+      Payload: JSON.stringify({ body: payload }),
       InvocationType: AWSInvokeType.fireAndForget
-    }
-
-    AWS.Lambda.prototype.invoke = jest.fn().mockReturnValue({
-      promise: jest.fn().mockReturnValue(Promise.resolve({}))
-    })
+    };
 
     const sut = new LambdaCloudService(context);
-    const response = await sut.invoke<Promise<any>>("aws-getCart", true, {});
+    const response = await sut.invoke<any>("aws-getCart", true, payload);
     expect(AWS.Lambda.prototype.invoke).toHaveBeenCalledWith(options);
-    expect(response).toEqual({});
-  })
+    expect(response).toBeUndefined();
+  });
 
   it("should return data on fireAndWait", async () => {
+    const payload = {};
     const options = {
-      FunctionName: "arn-west-us-2-getCart",
-      Payload: {},
+      FunctionName: getCartAWSCloudService.arn,
+      Payload: JSON.stringify({ body: payload }),
       InvocationType: AWSInvokeType.fireAndWait
-    }
+    };
 
     AWS.Lambda.prototype.invoke = jest.fn().mockReturnValue({
-      promise: jest.fn().mockReturnValue(Promise.resolve({ data: "some data" }))
-    })
+      createReadStream: jest.fn().mockReturnValue(invokeResponse)
+    });
 
     const sut = new LambdaCloudService(context);
-    const response = await sut.invoke<Promise<any>>("aws-getCart", false, {});
+    const response = await sut.invoke<any>("aws-getCart", false, payload);
     expect(AWS.Lambda.prototype.invoke).toHaveBeenCalledWith(options);
-    expect(response.data).toEqual("some data");
-  })
+    expect(response).toEqual(invokeResponseBody);
+  });
+
+  it("should throw an error if the stream fails", async () => {
+    AWS.Lambda.prototype.invoke = jest.fn().mockReturnValue({
+      createReadStream: jest.fn().mockImplementation(() => {
+        const file = new Readable({
+          read() {}
+        });
+        setImmediate(() => file.emit("error", new Error("fail")));
+        return file;
+      })
+    });
+
+    const payload = {};
+    const sut = new LambdaCloudService(context);
+
+    await expect(sut.invoke<any>("aws-getCart", false, payload)).rejects.toThrow("fail");
+  });
 
   it("should return an error if no name is passed", async () => {
     const sut = new LambdaCloudService(context);
-    try {
-      await sut.invoke<Promise<any>>(null, false, {})
-    } catch (err) {
-      expect(err).toEqual(Error("Name is needed"));
-    }
-  })
+    await expect(sut.invoke<any>(null, false, {})).rejects.toMatch("Name is needed");
+  });
 
   it("should return an error if no region or arn is passed", async () => {
     const awsUpdateCartApi: AWSCloudServiceOptions = {
-      name: "aws-updateCart",
+      name: getCartAWSCloudService.name,
       arn: undefined,
       region: undefined,
     };
@@ -97,7 +121,7 @@ describe("LambdaCloudService", () => {
     container.bind(awsUpdateCartApi.name).toConstantValue(awsUpdateCartApi);
 
     const sut = new LambdaCloudService(context);
-    const invoke = async () => await sut.invoke<Promise<any>>("aws-updateCart", false, {});
-    await expect(invoke()).rejects.not.toBeNull();
-  })
-})
+    await expect(sut.invoke<any>("aws-getCart", false, {})).rejects.toMatch("Region and ARN are needed for Lambda calls");
+  });
+
+});
