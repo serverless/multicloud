b3d50a81e38943fed4bc3d7789879671e32d4f6f
Lucas Palacios <34112271+Alaple@users.noreply.github.com>
2019-11-22 16:50:33 -0300

    feat: Add invoke interface to the CloudService (#46)
    Added invoke interface to be used in the azureFunctionCloudService. Updated the invoke function to use the new interface and unpdated the azure unit test to use the class.

--- core/src/services/cloudService.ts
+++ core/src/services/cloudService.ts
@@ -1,6 +1,27 @@
 import { StringParams } from "../common/stringParams";
 
 /**
+ * Invoke Request options
+ * Implemented in order to have all the parameters needed for the invoke function in one object
+ */
+export interface InvokeRequest {
+  /**
+   * @param name Function name to invoke
+   * @param fireAndForget Wait for response if false (default behavior)
+   * @param payload HTTP request body
+   * @param headers Context headers
+   * @param pathParams StringParams with values for URL
+   * @param extraParams Extra parameters to invoke function
+   */
+  name: string;
+  fireAndForget: boolean;
+  payload: any;
+  headers?: StringParams;
+  pathParams?: StringParams;
+  extraParams?: any;
+}
+
+/**
  * Options for Cloud Service invocation
  */
 export interface CloudServiceOptions {
@@ -13,9 +34,7 @@ export interface CloudServiceOptions {
 export interface CloudService {
   /**
    * Invoke a deployed Serverless function
-   * @param name Name of function to forget
-   * @param fireAndForget Don't listen for response if true
-   * @param payload Payload to send in invocation
+   * @param invokeOptions parameters needed to call the invoke
    */
-  invoke<T>(name: string, fireAndForget: boolean, payload?: any, headers?: StringParams): Promise<T>;
+  invoke<T>(invokeOptions: InvokeRequest): Promise<T>;
 }
