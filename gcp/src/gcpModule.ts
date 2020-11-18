4a8833fbdd0ba398bd72fb1479ae801b33563e7c
Elias Caceres <elias.caceres@southworks.com>
2020-11-18 15:01:05 -0300

    Model added

--- gcp/src/gcpModule.ts
+++ gcp/src/gcpModule.ts
@@ -1,8 +1,43 @@
-import { CloudModule } from "@multicloud/sls-core";
-
+import { CloudModule, ComponentType } from "@multicloud/sls-core";
+import { ContainerModule, interfaces } from "inversify";
 
 /**
  * GCP Module that can be registered in IoC container
  */
 export class GcpModule implements CloudModule {
+  /**
+   * Determines whether or not the incoming request is an AWS request
+   * @param req The IoC resolution request
+   */
+  private isGcpRequest(req: interfaces.Request) {
+    const runtimeArgs = req.parentContext.container.get(
+      ComponentType.RuntimeArgs
+    );
+    return runtimeArgs && runtimeArgs[1].eventId; //TBD gcpRequestId name
+  }
+
+  public create() {
+    return new ContainerModule((bind) => {
+      bind<CloudContext>(ComponentType.CloudContext)
+        .to(GcpContext)
+        .inSingletonScope()
+        .when(this.isGcpRequest);
+
+      bind<CloudRequest>(ComponentType.CloudRequest)
+        .to(GcpRequest)
+        .when(this.isGcpRequest);
+
+      bind<CloudResponse>(ComponentType.CloudResponse)
+        .to(GcpResponse)
+        .when(this.isGcpRequest);
+
+      // bind<CloudService>(ComponentType.CloudService)
+      //   .to(GcpFunctionCloudService)
+      //   .when(this.isGcpRequest);
+
+      // bind<CloudStorage>(ComponentType.CloudStorage)
+      //   .to(GcpBlobStorage)
+      //   .when(this.isGcpRequest);
+    });
+  }
 }
