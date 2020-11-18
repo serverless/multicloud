c2333f277746127cf9a69110b1170e7c4b5856e7
Fabio Orlinski <47789756+FabioOrlinski@users.noreply.github.com>
2020-11-18 12:31:02 -0300

    Added gcpContext

---
+++ gcp/src/gcpRequest.test.ts
@@ -0,0 +1,66 @@
+import { GcpContext, GcpRequest } from ".";
+import gcpEvent from "./test/events/defaultGcpEvent.json";
+import { StringParams } from "@multicloud/sls-core";
+
+describe("test of request", () => {
+  const context = {
+    requestId: "12345",
+    req: {},
+    res: {}
+  }
+
+  it("should pass-through event values without modifications", () => {
+    gcpEvent.body = JSON.stringify(gcpEvent.body);
+    const request = new GcpRequest(new GcpContext([gcpEvent, context, null]));
+    expect(request.method).toEqual(gcpEvent.httpMethod);
+    expect(request.headers).toEqual(new StringParams(gcpEvent.headers));
+    expect(request.query).toEqual(new StringParams(gcpEvent.queryStringParameters));
+    expect(request.body).toEqual(JSON.parse(gcpEvent.body));
+  });
+
+  it("should use default value for body if not provided", () => {
+    const noBodyEvent = Object.assign({}, gcpEvent);
+    delete noBodyEvent.body;
+    const request = new GcpRequest(new GcpContext([noBodyEvent, context, null]));
+    expect(request.method).toEqual(gcpEvent.httpMethod);
+    expect(request.headers).toEqual(new StringParams(gcpEvent.headers));
+    expect(request.query).toEqual(new StringParams(gcpEvent.queryStringParameters));
+    expect(request.body).toEqual(null);
+  });
+
+  it("should use default value for headers if not provided", () => {
+    const noHeadersEvent = Object.assign({}, gcpEvent);
+    delete noHeadersEvent.headers;
+    noHeadersEvent.body = JSON.stringify(noHeadersEvent.body);
+    const request = new GcpRequest(new GcpContext([noHeadersEvent, context, null]));
+    expect(request.method).toEqual(gcpEvent.httpMethod);
+    expect(request.headers).toEqual(new StringParams());
+    expect(request.query).toEqual(new StringParams(gcpEvent.queryStringParameters));
+    expect(request.body).toEqual(JSON.parse(noHeadersEvent.body));
+  });
+
+  it("should use default value for query if not provided", () => {
+    const noQueryEvent = Object.assign({}, gcpEvent);
+    delete noQueryEvent.queryStringParameters;
+    noQueryEvent.body = JSON.stringify(noQueryEvent.body);
+    const request = new GcpRequest(new GcpContext([noQueryEvent, context, null]));
+    expect(request.method).toEqual(gcpEvent.httpMethod);
+    expect(request.headers).toEqual(new StringParams(gcpEvent.headers));
+    expect(request.query).toEqual(new StringParams());
+    expect(request.body).toEqual(JSON.parse(noQueryEvent.body));
+  });
+
+  it("should set context defaults if context content are empty objects", () => {
+    const emptyParams = new StringParams();
+    const emptygcpEvent = {
+      httpMethod: "GET"
+    };
+
+    const request = new GcpRequest(new GcpContext([emptygcpEvent, context, null]));
+
+    expect(request.method).toEqual("GET");
+    expect(request.headers).toEqual(emptyParams);
+    expect(request.query).toEqual(emptyParams);
+    expect(request.body).toEqual(null);
+  });
+});
---
+++ gcp/src/index.ts
@@ -0,0 +1,9 @@
+<<<<<<< HEAD
+export * from "./gcpContext";
+export * from "./gcpRequest";
+export * from "./gcpResponse";
+=======
+export * from "./gcpModule";
+export * from "./gcpContext";
+export * from "./gcpRequest";
+>>>>>>> 7dd5d97... added test file
