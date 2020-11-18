861bb4d4349790d4cec62a27155aca84a8209dfb
Fabio Orlinski <47789756+FabioOrlinski@users.noreply.github.com>
2020-11-18 12:31:02 -0300

    Added gcpContext

--- gcp/src/index.ts
+++ gcp/src/index.ts
@@ -1,4 +1,7 @@
 export * from "./gcpContext";
 export * from "./gcpRequest";
 export * from "./gcpResponse";
+<<<<<<< HEAD
 export * from "./gcpModule";
+=======
+>>>>>>> a98564b... Added gcpContext
