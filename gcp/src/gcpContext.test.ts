5dfd3357217b0289b19d0a50cc79c50e3226ae49
Elias Caceres <elias.caceres@southworks.com>
2020-11-19 14:31:53 -0300

    test running

--- gcp/src/gcpContext.test.ts
+++ gcp/src/gcpContext.test.ts
@@ -4,11 +4,12 @@ const runtimeArgs = [
   {},
   {
     eventId: "12344",
-  }
+  },
+  jest.fn(),
 ];
 
 const createGcpContext = (args): GcpContext => {
-  const gcpContext = new GcpContext(args);
+  let gcpContext = new GcpContext(args);
   gcpContext.res = new GcpResponse(gcpContext);
   gcpContext.done = jest.fn();
   return gcpContext;
@@ -23,8 +24,25 @@ describe("GCP context", () => {
   it("when send() calls response.send() on httpTrigger with custom status", () => {
     const context = createGcpContext(runtimeArgs);
     const body = { message: "oh Crap!" };
-    context.send(body, 400);
-    console.log(context);
-    expect(context.res.send).toBeCalledWith(body, 400);
+    context.res = new GcpResponse(context);
+    context.res.send = jest.fn();
+    context.send(body);
+    expect(context.res.send).toBeCalledWith(body, 200);
+  });
+
+  it("send() calls context.done() to signal handler is complete", () => {
+    const context = createGcpContext(runtimeArgs);
+    context.send("test", 200);
+
+    expect(context.done).toBeCalled();
+  });
+
+  it("flush() calls response.flush() to call final AWS callback", () => {
+    const context = createGcpContext(runtimeArgs);
+    const flushSpy = jest.spyOn(context.res, "flush");
+    context.send("test", 200);
+    context.flush();
+
+    expect(flushSpy).toBeCalled();
   });
 });
