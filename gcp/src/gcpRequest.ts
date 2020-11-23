36f300f42ec9354e73f7a1f2fd2c16aaac4b88c5
Fabio Orlinski <47789756+FabioOrlinski@users.noreply.github.com>
2020-11-23 16:45:37 -0300

    Added parseJson function to gcpRequest.ts

--- gcp/src/gcpRequest.ts
+++ gcp/src/gcpRequest.ts
@@ -20,17 +20,32 @@ export class GcpRequest implements CloudRequest {
   public pathParams?: StringParams;
 
   /**
+   * Parse the body
+   * @param body Body of HTTP request
+   */
+  private parseJson(body: any) {
+    let parsedBody;
+    try{
+      parsedBody = JSON.parse(body);
+    } catch (e) {
+      parsedBody = null;
+    }
+    return parsedBody;
+  }
+
+  /**
    * Initialize new Gcp Request, injecting Cloud Context
    * @param context Current CloudContext
    */
   public constructor(@inject(ComponentType.CloudContext) context: GcpContext) {
     const req = context.runtime.event;
+    const body = this.parseJson(req.body);
 
-
-    this.body = req.body ? JSON.parse(req.body) : null;
+    this.body = body;
     this.headers = new StringParams(req.headers);
     this.method = req.method || "";
     this.query = new StringParams(req.query);
     this.pathParams = new StringParams(req.params);
   }
 }
+
